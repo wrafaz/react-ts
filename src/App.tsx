908eabb91e2a6bc399273decd179ff938a6e299b
Rafael Aguilar Rodriguez <rafael.aguilarrodriguez@travelport.com>
2020-11-23 09:28:18 +0100

    Createing the data store part II

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,32 @@
 import React, { Component } from 'react';
-import { Product, Order } from './data/entities';
-import { ProductList } from './productList';
-
-let testData: Product[] = [1, 2, 3, 4, 5].map(num =>
-  ({
-    id: num,
-    name: `Prod${num}`,
-    category: `Cat${num % 2}`,
-    description: `Product ${num}`,
-    price: 100
-  }))
+//import { Product, Order } from './data/entities';
+//import { ProductList } from './productList';
+import { dataStore } from "./data/dataStore";
+import { Provider } from 'react-redux';
+import { HttpHandler } from "./data/httpHandler";
+import { addProduct } from './data/actionCreators';
+import { ConnectedProductList } from './data/productListConnector';
 
 interface Props {
   // no props required
 }
 
-interface State {
-  order: Order
-}
-
-export default class App extends Component<Props, State> {
+export default class App extends Component<Props> {
+  private httpHandler = new HttpHandler();
 
   constructor(props: Props) {
     super(props);
-    this.state = {
-      order: new Order()
-    }
+    this.httpHandler.loadProducts(data =>
+      dataStore.dispatch(addProduct(...data)));
   }
-
-  get categories(): string[] {
-    return [...new Set(testData.map(p => p.category))]
-  }
-
-  addToOrder = (product: Product, quantity: number) => {
-    this.setState(state => {
-      state.order.addProduct(product, quantity);
-      return state;
-    })
-  }
-
+  
   render = () =>
     <div className="App">
-      <ProductList 
-        products={ testData }
-        categories={ this.categories }
-        order={ this.state.order }
-        addToOrder={ this.addToOrder } />
+      <Provider store={dataStore}>
+        <ConnectedProductList />
+      </Provider>
     </div>
+  submitCallback = () => {
+    console.log("Submit order");
+  }
 }
