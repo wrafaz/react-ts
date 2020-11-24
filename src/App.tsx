63fa3b67de33968db412bfc2de83f58793133fb8
Rafael Aguilar Rodriguez <rafael.aguilarrodriguez@travelport.com>
2020-11-24 08:40:34 +0100

    completing example application features

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import { Provider } from 'react-redux';
 import { HttpHandler } from "./data/httpHandler";
 import { addProduct } from './data/actionCreators';
 import { ConnectedProductList } from './data/productListConnector';
+import { Switch, Route, Redirect, BrowserRouter, RouteComponentProps } from "react-router-dom";
+import { OrderDetails } from './orderDetails';
+import { Summary } from './summary';
 
 interface Props {
   // no props required
@@ -19,14 +22,26 @@ export default class App extends Component<Props> {
     this.httpHandler.loadProducts(data =>
       dataStore.dispatch(addProduct(...data)));
   }
-  
+
   render = () =>
     <div className="App">
       <Provider store={dataStore}>
-        <ConnectedProductList />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/products" component={ConnectedProductList} />
+            <Route path="/order" render={(props) =>
+              <OrderDetails {...props} submitCallback={() =>
+                this.submitCallback(props)} />
+            } />
+            <Route path="/summary/:id" component={Summary} />
+            <Redirect to="/products" />
+          </Switch>
+        </BrowserRouter>
       </Provider>
     </div>
-  submitCallback = () => {
-    console.log("Submit order");
+
+  submitCallback = (routeProps: RouteComponentProps) => {
+    this.httpHandler.storeOrder(dataStore.getState().order,
+      id => routeProps.history.push(`/summary/${id}`));
   }
 }
--- src/header.tsx
+++ src/header.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Order } from "./data/entities";
+import { NavLink } from "react-router-dom"
 
 interface Props {
     order: Order
@@ -11,9 +12,9 @@ export class Header extends Component<Props> {
         let count = this.props.order.productCount;
         return <div className="p-1 bg-secondary text-white text-right">
             { count === 0 ? "(No Selection)" : `${ count } product(s), $${ this.props.order.total.toFixed(2) }` }
-            <button className="btn btn-sm btn-primary m-1">
+            <NavLink to="/order" className="btn btn-sm btn-primary m-1">
                 Submit Order
-            </button>
+            </NavLink>
         </div>
     }
 }
